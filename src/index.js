47fa67adc18475111a1c061a8fce68f67e19d654
DariaDrozd <reddymargot@gmail.com>
2023-07-01 13:41:13 +0300

    fixed all bugs

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import './index.css';
+import { GlobalStyle } from 'components/GlobalStyle';
 import { Provider } from 'react-redux';
-import { store, persistor} from 'store/store';
 import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-          <App />
+        <GlobalStyle />
+        <App />
       </PersistGate>
     </Provider>
   </React.StrictMode>
